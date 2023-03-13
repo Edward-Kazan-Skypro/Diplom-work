c97554414f60b81b38e8515c9e11cfb905ccc20d
ViharevN <swix_off@mail.ru>
2023-03-13 11:06:11 +0300

    add project

---
+++ src/main/java/pro/sky/finalprojectsky/FinalProjectSkyApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.finalprojectsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectSkyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectSkyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/finalprojectsky/FinalProjectSkyApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.finalprojectsky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectSkyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
