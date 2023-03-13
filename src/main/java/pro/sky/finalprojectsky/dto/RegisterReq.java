4f551ccd24d58aa41e1e5cc24b3dda540c886f78
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2023-03-13 17:21:42 +0300

    Добавил классы из образца, исправил импорты

---
+++ src/main/java/pro/sky/finalprojectsky/dto/RegisterReq.java
@@ -0,0 +1,13 @@
+package pro.sky.finalprojectsky.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterReq {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private Role role;
+}
---
+++ src/main/java/pro/sky/finalprojectsky/service/impl/AuthServiceImpl.java
@@ -0,0 +1,50 @@
+package pro.sky.finalprojectsky.service.impl;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
+import pro.sky.finalprojectsky.dto.RegisterReq;
+import pro.sky.finalprojectsky.dto.Role;
+import pro.sky.finalprojectsky.service.AuthService;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private final UserDetailsManager manager;
+
+    private final PasswordEncoder encoder;
+
+    public AuthServiceImpl(UserDetailsManager manager) {
+        this.manager = manager;
+        this.encoder = new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public boolean login(String userName, String password) {
+        if (!manager.userExists(userName)) {
+            return false;
+        }
+        UserDetails userDetails = manager.loadUserByUsername(userName);
+        String encryptedPassword = userDetails.getPassword();
+        String encryptedPasswordWithoutEncryptionType = encryptedPassword.substring(8);
+        return encoder.matches(password, encryptedPasswordWithoutEncryptionType);
+    }
+
+    @Override
+    public boolean register(RegisterReq registerReq, Role role) {
+        if (manager.userExists(registerReq.getUsername())) {
+            return false;
+        }
+        manager.createUser(
+                User.withDefaultPasswordEncoder()
+                        .password(registerReq.getPassword())
+                        .username(registerReq.getUsername())
+                        .roles(role.name())
+                        .build()
+        );
+        return true;
+    }
+}
