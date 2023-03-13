823a4c7fdaf897fcdc6fb4a2c2fc1be7872ce6ed
OlgaSmi <ol.zaigraewa@yandex.ru>
2023-03-13 18:03:43 +0300

    Initial commit

--- src/main/java/pro/sky/finalprojectsky/FinalProjectSkyApplication.java
+++ src/main/java/pro/sky/finalprojectsky/FinalProjectSkyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FinalProjectSkyApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(FinalProjectSkyApplication.class, args);
     }
