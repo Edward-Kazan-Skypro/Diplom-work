7528280d85ef678e67e586158a525c53113eb754
Alexey <alexey_kuzminykh@mail.ru>
2023-03-20 20:15:43 +0300

    Добавил поля CreateAdsDTO, AdsDTO и openAPI

--- src/main/java/pro/sky/finalprojectsky/dto/AdsDTO.java
+++ src/main/java/pro/sky/finalprojectsky/dto/AdsDTO.java
@@ -1,4 +1,18 @@
 package pro.sky.finalprojectsky.dto;
 
+import lombok.Data;
+
+@Data
 public class AdsDTO {
+    private int pk;
+
+    private int author;
+
+    private String image;
+
+    private int price;
+
+    private String title;
+
+    private String description;
 }
--- src/main/java/pro/sky/finalprojectsky/dto/CreateAdsDTO.java
+++ src/main/java/pro/sky/finalprojectsky/dto/CreateAdsDTO.java
@@ -1,4 +1,13 @@
 package pro.sky.finalprojectsky.dto;
 
+import lombok.Data;
+
+@Data
 public class CreateAdsDTO {
+
+    private String description;
+
+    private int price;
+
+    private String title;
 }
