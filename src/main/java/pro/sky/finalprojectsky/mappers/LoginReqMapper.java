01176ca3ceb3ec3c696c9d3181a21b2959e7bef5
Vasiliy_Khotenov <woodoo08@mail.ru>
2023-03-28 00:22:16 +0300

    Заполнил мапперы LoginReqMapper, NewPasswordMapper и RegisterReqMapper.

--- src/main/java/pro/sky/finalprojectsky/mappers/LoginReqMapper.java
+++ src/main/java/pro/sky/finalprojectsky/mappers/LoginReqMapper.java
@@ -1,6 +1,8 @@
 package pro.sky.finalprojectsky.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 import pro.sky.finalprojectsky.dto.LoginReqDto;
 import pro.sky.finalprojectsky.model.User;
@@ -8,6 +10,16 @@ import pro.sky.finalprojectsky.model.User;
 @Mapper
 public interface LoginReqMapper {
     LoginReqMapper INSTANCE = Mappers.getMapper(LoginReqMapper.class);
+
+    @Mappings({
+            @Mapping(target = "password", source = "userEntity.password"),
+            @Mapping(target = "username", source = "userEntity.userName")
+    })
     LoginReqDto userToDto (User userEntity);
+
+    @Mappings({
+            @Mapping(target = "password", source = "loginReqDto.password"),
+            @Mapping(target = "username", source = "loginReqDto.userName")
+    })
     User dtoToEntity (LoginReqDto loginReqDto);
 }
--- src/main/java/pro/sky/finalprojectsky/mappers/RegisterReqMapper.java
+++ src/main/java/pro/sky/finalprojectsky/mappers/RegisterReqMapper.java
@@ -1,6 +1,8 @@
 package pro.sky.finalprojectsky.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 import pro.sky.finalprojectsky.dto.RegisterReqDto;
 import pro.sky.finalprojectsky.model.User;
@@ -8,7 +10,16 @@ import pro.sky.finalprojectsky.model.User;
 @Mapper
 public interface RegisterReqMapper {
     RegisterReqMapper INSTANCE = Mappers.getMapper(RegisterReqMapper.class);
+
+    @Mappings({
+            @Mapping(target = "username", source = "userName"),
+            @Mapping(target = "lastName", source = "secondName")
+    })
     RegisterReqDto userToDto (User userEntity);
 
+    @Mappings({
+            @Mapping(target = "userName", source = "username"),
+            @Mapping(target = "secondName", source = "lastName")
+    })
     User dtoToEntity (RegisterReqDto registerRegDto);
 }
