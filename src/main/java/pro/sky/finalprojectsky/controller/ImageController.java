29a20e35d81413f9afa18cfac309bce2f3e4b692
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2023-03-31 21:20:44 +0300

    Добавил в мапперы аннотации, чтобы игнорировать невозможность конвертации Image в String - при маппинге полей Юзера и Объявления в соответствующие ДТО.

--- src/main/java/pro/sky/finalprojectsky/controller/ImageController.java
+++ src/main/java/pro/sky/finalprojectsky/controller/ImageController.java
@@ -6,10 +6,12 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 //@Slf4j
--- src/main/java/pro/sky/finalprojectsky/model/User.java
+++ src/main/java/pro/sky/finalprojectsky/model/User.java
@@ -40,7 +40,7 @@ public class User {
     //здесь - связь между аватаркой пользователя и пользователем
     @OneToOne
     @JoinColumn(name = "avatar_id")
-    private Avatar avatar;
+    private Avatar image;
 }
 
 
