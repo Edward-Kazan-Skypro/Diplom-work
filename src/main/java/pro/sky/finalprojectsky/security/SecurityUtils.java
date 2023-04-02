ae9302a494251d10301bf521a974ce9109d0098a
yura178500 <yura178500@yandex.ru>
2023-04-02 14:01:39 +0300

    пороботал с DTO ,сделал сервис комментов(мне кажется надо обьеденить обьявления и комменты), сделал енити,добавил обработку картинки,

---
+++ src/main/java/pro/sky/finalprojectsky/security/SecurityUtils.java
@@ -0,0 +1,38 @@
+package pro.sky.finalprojectsky.security;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import pro.sky.finalprojectsky.dto.Role;
+import pro.sky.finalprojectsky.entity.Ads;
+import pro.sky.finalprojectsky.entity.AdsComment;
+
+import java.nio.file.AccessDeniedException;
+
+public class SecurityUtils {
+
+    private SecurityUtils() {
+    }
+
+    public static MyUserDetails getUserDetailsFromContext() {
+        return (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    public static long getUserIdFromContext() {
+        return getUserDetailsFromContext().getId();
+    }
+
+    public static void checkPermissionToAds(Ads ads) throws AccessDeniedException {
+        MyUserDetails userDetails = getUserDetailsFromContext();
+
+        if (!userDetails.getAuthorities().contains(Role.ADMIN) && userDetails.getId() != ads.getAuthor().getId()) {
+            throw new AccessDeniedException("Чтобы изменить/удалить объявление, нужно иметь роль ADMIN или быть владельцем этого объявления");
+        }
+    }
+
+    public static void checkPermissionToAdsComment(AdsComment adsComment) throws AccessDeniedException {
+        MyUserDetails userDetails = getUserDetailsFromContext();
+
+        if (!userDetails.getAuthorities().contains(Role.ADMIN) && userDetails.getId() != adsComment.getAuthor().getId()) {
+            throw new AccessDeniedException("Чтобы изменить/удалить комментарий, нужно иметь роль ADMIN или быть владельцем этого комментария");
+        }
+    }
+}
