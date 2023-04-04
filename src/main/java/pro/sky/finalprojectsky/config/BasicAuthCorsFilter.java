e846e0114d580f1bb31a0fcd56eea7914701108c
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2023-04-04 18:02:36 +0300

    Убрал лишнее из импорта в BasicAuthCorsFilter

--- src/main/java/pro/sky/finalprojectsky/config/BasicAuthCorsFilter.java
+++ src/main/java/pro/sky/finalprojectsky/config/BasicAuthCorsFilter.java
@@ -1,22 +1,14 @@
 package pro.sky.finalprojectsky.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
+
 
 @Component
 public class BasicAuthCorsFilter extends OncePerRequestFilter {
